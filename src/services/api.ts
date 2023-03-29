637799e86029b7e73ad3bc9ed428e8869b22d5ee
Adryan Frey <adryanfrey886@gmail.com>
2023-03-29 18:19:33 +0200

    changed api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ export function setUpApiClient(ctx: any){
     let cookies = parseCookies(ctx)
     
     const api = axios.create({
-        baseURL: 'http://localhost:3333',
+        baseURL: 'https://barber-pro-backend.cyclic.app',
         headers: {
             Authorization: `Bearer ${cookies['@barberProToken']}`
         }
